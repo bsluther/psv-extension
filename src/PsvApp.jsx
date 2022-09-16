69c65a09b428aa57e0aa8ba3e5b1380e67533f1e
bsluther <briansluther@gmail.com>
2022-09-16 16:43:09 -0600

    Preliminary version working, more or less.

--- src/PsvApp.jsx
+++ src/PsvApp.jsx
@@ -1,11 +1,17 @@
 import './PsvApp.css'
 import 'photo-sphere-viewer/dist/photo-sphere-viewer.css'
 import 'photo-sphere-viewer/dist/plugins/markers.css'
+import { createRoot } from 'react-dom/client'
+import { createPortal } from 'react-dom'
 import { Viewer } from 'photo-sphere-viewer'
 import { MarkersPlugin } from 'photo-sphere-viewer/dist/plugins/markers'
 import { useEffect, useState } from 'react'
 import { useRef } from 'react'
 import redSwab from './assets/redSwab.png'
+import { lensPath, map, over, prop, values } from 'ramda'
+import { useCallback } from 'react'
+import { v4 as uuid } from 'uuid'
+import { pencilAlt } from './svg/pencilAlt'
 
 let called = false
 const callOnce = fn => {
@@ -15,122 +21,294 @@ const callOnce = fn => {
   }
 }
 
+
+
 const initialMarkersData = [
   {
-    id: 'marker-1',
+    id: 'ae64ecba-51fc-4ee1-8305-7ff426b84a46',
     latitude: '40deg',
     longitude: '40deg',
     image: redSwab,
     height: 50,
-    width: 50
+    width: 50,
+    data: {
+      dataEntryComplete: true,
+      label: 'Test Site 1',
+      description: 'This is the first test site.'
+    }
   },
   {
-    id: 'marker-2',
+    id: '95570ac9-fd46-4173-b4b7-e682a4428d6a',
     latitude: '20deg',
     longitude: '35deg',
     image: redSwab,
     height: 50,
-    width: 50
+    width: 50,
+    data: {
+      dataEntryComplete: true,
+      label: 'Smallpox finding',
+      description: 'This is the first test site where we found smallpox'
+    }
   },
   {
-    id: 'marker-3',
+    id: '699914d4-fdd4-4e26-be4a-a8357f3f06c0',
     latitude: '0deg',
     longitude: '35deg',
     image: redSwab,
     height: 50,
-    width: 50
+    width: 50,
+    data: {
+      dataEntryComplete: false,
+      label: 'Gross',
+      description: 'Really not sure what that is...'
+    }
   },
 ]
+
+const makeMarker = ({ latitude, longitude }) => ({ 
+  id: uuid(),
+  latitude,
+  longitude,
+  image: redSwab,
+  height: 50,
+  width: 50,
+  data: {
+    dataEntryComplete: false
+  }
+})
+
+
 
 export const PsvApp = () => {
   const viewerContainerRef = useRef()
   const [psv, setPsv] = useState()
   const [markersInstance, setMarkersInstance] = useState()
-  const markersPanelRef = useRef()
+  const [markersState, setMarkersState] = useState({})
+  const [panelOpen, setPanelOpen] = useState(false)
+  const [markerFocus, setMarkerFocus] = useState()
 
   useEffect(() => {
     if (viewerContainerRef.current && !psv) {
       callOnce(() => {
-        const instance = new Viewer({
+        const psvInstance = new Viewer({
           container: viewerContainerRef.current,
           panorama: './r1p4.jpg',
           plugins: [
             [MarkersPlugin, {
               markers: initialMarkersData
             }]
+          ],
+          navbar: [
+            'autorotate',
+            'zoom',
+            'move',
+            {
+              id: 'custom-markers-panel',
+              content: pencilAlt,
+              title: 'Custom Markers Panels',
+              className: 'w-5 h-5',
+              onClick: viewer => {
+                setPanelOpen(prev => !prev)
+              }
+            },
+            'fullscreen'
           ]
         })
-        
-        setPsv(instance)
+
+        const markersPlugin = psvInstance.getPlugin(MarkersPlugin)
+        setMarkersInstance(markersPlugin)
+        const markersProxy = new Proxy(markersPlugin.markers, {
+          set: (prev, key, value ) => {
+            setMarkersState(Object.assign({}, prev, { [key]: value }))
+            return Object.assign(prev, { [key]: value })
+          }
+        })
+        markersPlugin.markers = markersProxy
+
+        markersPlugin.on('select-marker', (event, data, click) => {
+          setMarkerFocus(data.id)
+        })
+
+        markersPlugin.on('unselect-marker', (event, data, click) => {
+          console.log('unselected')
+        })
+
+        psvInstance.on('click', (event, data) => {
+          markersPlugin.addMarker(makeMarker({ latitude: data.latitude, longitude: data.longitude }))
+        })
+
+        setPsv(psvInstance)
       })
     }
   }, [psv, viewerContainerRef])
 
-  useEffect(() => {
-    if (!!psv && !markersInstance) {
-      setMarkersInstance(psv.getPlugin(MarkersPlugin))
-    }
-  }, [!!psv, !markersInstance])
+
   
   useEffect(() => {
     if (!!psv && !!markersInstance) {
-      psv.panel.show({
-        id: 'markers-panel',
-        width: '40%',
-        content: markersPanelRef.current.innerHTML
-      })
+      if (panelOpen) {
+        psv.panel.show({ 
+          id: 'markers-panel',
+          width: '40%',
+          content: "<div id='panel-content-root'>content</div>"
+        })
+        const panelContentRoot = document.getElementById('panel-content-root')
+        createRoot(panelContentRoot).render(
+          <MarkerPanel psv={psv} markersState={markersState} markersInstance={markersInstance} />
+        )
+      } else {
+        psv.panel.hide('markers-panel')
+      }
     }
-  }, [!!psv, !!markersInstance])
-  console.log('markers instance', markersInstance)
+  }, [!!psv, !!markersInstance, markersState, panelOpen]) 
+
+
+  const handleUpdateData = useCallback(key => updater => markerId => {
+    const newMarkersState = over(lensPath([markerId, 'config', 'data', key]))
+                                (updater)
+                                (markersState)
+
+    const newMarkersConfigs = map(prop('config'))
+                                 (values(newMarkersState))
+
+    markersInstance.setMarkers(newMarkersConfigs)
+  }, [markersInstance, markersState])
+
+  const handleDeleteMarker = useCallback(markerId => {
+    markersInstance.removeMarker(markerId)
+  }, [markersInstance])
+ 
   return (
-    <div className='psv-app'>
+    <div className='w-screen h-screen'>
       <div
-        className='viewer-container'
+        className='w-full h-full'
         ref={viewerContainerRef}
       />
+ 
 
-      <button
-        onClick={() => {
-          markersInstance.addMarker(makeMarker({ id: Math.random(), latitude: '20deg', longitude: '20deg' }))
-        }}
-      >add marker</button>
+      {markerFocus &&
+        <MarkerFocus 
+          id={markerFocus} 
+          markersState={markersState} 
+          handleUpdateData={handleUpdateData} 
+          setMarkerFocus={setMarkerFocus} 
+          handleDeleteMarker={handleDeleteMarker}
+        />
+      }
+    </div>
+  )
+}
+ 
 
-      <div style={{ visibility: 'hidden' }} ref={markersPanelRef}>
-        {markersInstance && <MarkerPanel psv={psv} markersInstance={markersInstance} />}
-      </div>
+const MarkerPanel = ({ psv, markersInstance, markersState = {} }) => {
+  
+  const markers = Object.values(markersState)
+
+
+  const handleToggleEntryComplete = useCallback(marker => {
+    const newMarkersState = over(lensPath([marker.id, 'config', 'data', 'dataEntryComplete']))
+                                (prev => !prev)
+                                (markersState)
+
+    const newMarkersConfigs = map(prop('config'))
+                                 (values(newMarkersState))
+
+    markersInstance.setMarkers(newMarkersConfigs)
+  }
+  , [markersInstance, markersState])
+
+  const gotoMarker = (id, speed) => markersInstance.gotoMarker(id, speed)
+
+  return ( 
+    <div className='w-full space-y-4'>
+      {map(mrkr => 
+            <MarkerItem 
+              key={mrkr.id} 
+              marker={mrkr} 
+              handleToggleEntryComplete={handleToggleEntryComplete} 
+              markersState={markersState}
+              gotoMarker={gotoMarker}
+            />)
+          (values(markers))}
     </div>
   )
 }
 
-const makeMarker = ({ id, latitude, longitude }) => ({
-  id,
-  latitude,
-  longitude,
-  image: redSwab,
-  height: 50,
-  width: 50
-})
-
-const proxyHandler = {
-  get(target, prop, receiver) {
-    return "world"
-  },
-  addMarker: () => {
-    console.log('PROXYING!')
-  }
+const MarkerItem = ({ marker, handleToggleEntryComplete, gotoMarker }) => {
+  return (
+    <div className='w-full flex flex-col space-x-2 even:bg-blend-darken'>
+      <span 
+        onClick={() => gotoMarker(marker.id, 500)}
+      >
+        {marker.data.label.length > 0 ? marker.data.label : `ID: ${marker.id.slice(0,6)}...`}
+      </span>
+      <div className='space-x-2'>
+        <input type='checkbox' checked={marker.data.dataEntryComplete} onChange={() => handleToggleEntryComplete(marker)} />
+        <span>Data Entry Complete</span>
+      </div>
+      {/* <div className='space-x-2'>
+        <span>Description:</span>
+        <span>{marker.description}</span>
+      </div> */}
+    </div>
+  )
 }
 
-const MarkerPanel = ({ psv, markersInstance }) => {
-  const [proxy, setProxy] = useState(() => new Proxy(markersInstance, proxyHandler))
-  useEffect(() => {
-    if (proxy) {
-      console.log('psv', psv.plugins)
-      console.log('proxy', proxy)
-      psv.plugins.markers = proxy
-      console.log(psv.plugins.markers.markers)
-    }
-  }, [psv])
-  return (
-    <div>Testing</div>
+
+
+
+const MarkerFocus = ({ id, markersState, handleUpdateData, setMarkerFocus, handleDeleteMarker }) => {
+  const markerEl = document.getElementById(`psv-marker-${id}`)
+
+  const label = markersState[id]?.data.label ?? ""
+  // const description = markersState[id].data.description ?? ""
+
+  if (!markerEl) {
+    return null
+  }
+  return createPortal((
+    <div
+      className='absolute top-1/2 right-full -translate-x-2 -translate-y-1/2 bg-blue-800 rounded-md flex flex-col p-2 space-y-2 z-50'
+    >
+      <div className='flex w-max items-center space-x-2'>
+        <span className='text-white font-bold'>
+          Label
+        </span>
+        <input 
+          autoFocus 
+          value={label} 
+          className='bg-blue-300 rounded-md outline-none w-max p-1'
+          onChange={e => handleUpdateData('label')(() => e.target.value)(id)} 
+        />
+      </div>
+
+      {/* <div className='flex items-center space-x-2'>
+        <span className='text-white font-bold'>
+          Description
+        </span>
+        <textarea 
+          value={description} 
+          className='bg-blue-300 rounded-md outline-none h-20 p-1'
+          onChange={e => handleUpdateData('description')(() => e.target.value)(id)} 
+        />
+      </div> */}
+
+      <div className='w-full justify-center space-x-2 flex'>
+
+        <button
+          className='bg-white text-red-600 font-bold uppercase w-max py-1 px-2 rounded-md'
+          onClick={() => handleDeleteMarker(id)}
+        >delete</button>
+        <span className='grow' />
+        <button
+          className='bg-white font-bold uppercase w-max py-1 px-2 rounded-md'
+          onClick={() => setMarkerFocus(null)}
+        >done</button>
+
+      </div>
+
+      
+    </div>
   )
+  , markerEl)
 }
